1caa9f4915fa0b9056c621e42d737e6a7c3e4873
muszynskim <MuszynskiM@quindell.com>
2014-11-18 14:21:14 +0000

    improved int conversion

--- src/main/scala/com/mimu/HttpUtil.scala
+++ src/main/scala/com/mimu/HttpUtil.scala
@@ -3,6 +3,8 @@ package com.mimu
 import org.vertx.scala.core.http._
 import org.vertx.scala.core.MultiMap
 
+import scala.util.Try
+
 /**
  * Created by mm on 13/11/2014.
  */
@@ -17,14 +19,7 @@ object HttpUtil {
   def getIntParam(params: MultiMap, paramName: String, defaultValue: Int) : Int = {
     val s:scala.collection.mutable.Set[String] = params.getOrElse(paramName, scala.collection.mutable.Set[String]())
     val e:Option[String] = s.find( _ => true)
-    try {
-      e.getOrElse(defaultValue.toString).toInt
-    }
-    catch {
-      case e: NumberFormatException => {
-        defaultValue
-      }
-    }
+    Try(e.getOrElse("").toInt).getOrElse(defaultValue)
   }
 
 }
